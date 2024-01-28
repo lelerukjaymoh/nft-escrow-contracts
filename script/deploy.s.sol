b84cb0d8b64952dd306d2380e5ec13424e19ee29
Jay <lelerukjaymoh@gmail.com>
2024-01-29 00:51:51 +0300

    chore: deploy escrow smart contracts

--- script/deploy.s.sol
+++ script/deploy.s.sol
@@ -1,17 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
+// import {Asset} from "@immutablex/Asset.sol";
 import "forge-std/Script.sol";
-import {Asset} from "@immutablex/Asset.sol";
+import {NFTEscrow} from "@escrow/NFTEscrow.sol";
 
 contract DeployScript is Script {
     function run() public {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
 
-        address owner = vm.envAddress("OWNER");
-
-        new Asset(owner, "ImmutableX", "IMX", address(0x0);
+        new NFTEscrow();
 
         vm.stopBroadcast();
     }
